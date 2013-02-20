5736a23e8ad768c34c6db9af54cf2fc827410e05
= <=>
2013-02-20 13:47:47 -0800

    moving asteroids!

--- asteroid.js
+++ asteroid.js
@@ -1,63 +1,94 @@
 var AS = (function () {
 
-  function Asteroid(aX, aY) {
-    var x = aX;
-    var y = aY;
-    var size = Math.floor(Math.random() * 100 + 20);
+  function Asteroid(x, y, size, dir) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.dir = dir;
+    var that = this;
 
-    var createRandomAsteroid = function() {
-      var x = Math.floor(Math.random() * 800);
-      var y = Math.floor(Math.random() * 800);
-      return new Asteroid(x,  y);
+    this.draw = function(ctx) {
+      ctx.fillStyle="rgba(200, 200, 200, 0.7)";
+      ctx.beginPath();
+      ctx.arc(that.x, that.y, that.size, 0, Math.PI*2);
+      ctx.fill();
     };
 
-    var draw = function() {
-     fillStyle="rgba(200, 200, 200, 0.7)";
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
-      ctx.fill(); 
+    this.update = function() {
+      that.x = that.x + dir[0];
+      that.y = that.y + dir[1];
     };
   }
 
-  function Game(canvas) {
+
+  Asteroid.randomAsteroid = function (){
+    var x = Math.floor(Math.random() * 800);
+    var y = Math.floor(Math.random() * 800);
+    var size = Math.floor(Math.random() * 80 + 20);
+    var dir = [(Math.floor(Math.random() * 14 - 7 )), (Math.floor(Math.random() * 14 - 7))];
+    return new Asteroid(x, y, size, dir);
+  };
+
+  function Game(ctx) {
     var asteroids = [];
-
+    var that = this;
     for (var i=0; i < 10; i++) {
-      asteroids[i] = Asteroid.createRandomAsteroid();
+      asteroids[i] = Asteroid.randomAsteroid();
     }
 
-    var draw = function() {
+
+
+    this.draw = function() {
+      ctx.save();   // this is how you clear the board baby
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.clearRect(0, 0, 800, 800);
+      ctx.restore();
       for (var i=0; i < asteroids.length; i++) {
-        asteroids[i].draw();
-      }  
-    }
-}
+        asteroids[i].draw(ctx);
+      }
+    };
 
-    return { Asteroid: Asteroid };
+    this.update = function() {
+      for (var i = asteroids.length - 1; i >= 0; i--) {
+        asteroids[i].update();
+      }
+    };
+
+    this.start = function () {
+      that.draw();
+
+      setInterval(function() { that.update(); that.draw(); }, 31);
+      };
+    }
+
+    return { Asteroid: Asteroid,
+              Game: Game };
 })();
-
-
-
-
-var ctx = document.getElementById("asterCanvas").getContext("2d");
+var ctx= document.getElementById("asterCanvas").getContext("2d");
 var g = new AS.Game(ctx);
-ctx.fillStyle="rgba(200, 200, 200, 0.7)";
-ctx.beginPath();
-ctx.arc(300, 575, 40, 0, Math.PI*2);
-// ctx.arc(95,50,40,0,2*Math.PI);
-ctx.fill();
 
-ctx.fillStyle="rgba(0, 0, 255, 0.7)";
-ctx.beginPath();
-ctx.moveTo(225, 125);
-ctx.lineTo(225, 45);
-ctx.lineTo(145, 125);
-ctx.closePath();
-ctx.fill();
+g.start();
 
-    asteroids = [];
 
-    for (var i=0; i < 11; i++) {
-      asteroids[i] = AS.Asteroid.randomAsteroid();
-    }
-// ctx.stroke();
+
+
+// ctx.fillStyle="rgba(200, 200, 200, 0.7)";
+// ctx.beginPath();
+// ctx.arc(300, 575, 40, 0, Math.PI*2);
+// // ctx.arc(95,50,40,0,2*Math.PI);
+// ctx.fill();
+
+// ctx.fillStyle="rgba(0, 0, 255, 0.7)";
+// ctx.beginPath();
+// ctx.moveTo(225, 125);
+// ctx.lineTo(225, 45);
+// ctx.lineTo(145, 125);
+// ctx.closePath();
+// ctx.fill();
+
+//     asteroids = [];
+
+//     for (var i=0; i < 11; i++) {
+//       asteroids[i] = AS.Asteroid.randomAsteroid();
+//     }
+// // ctx.stroke();
